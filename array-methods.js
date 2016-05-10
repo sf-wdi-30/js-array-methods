3fae9df94d054c3d22f4a74d8380c0d5be559d54
Justin Castilla <justincastilla@users.noreply.github.com>
2016-05-10 12:09:09 -0700

    Create array-methods.js

---
+++ array-methods.js
@@ -0,0 +1,65 @@
+// Solutions - Array Manipulation
+
+/* Challenge Set A */
+console.log("\nChallenge Set A");
+
+/* Challenge 1
+Use your chrome console `ctrl+opt+j` to perform the following activities.
+Each of the following tasks can be accomplished using a single Array method in 1 line of code.
+*/
+console.log("\nChallenge 1.0");
+
+//TODO: Enter your code here!
+
+
+/* Challenge 2 */
+console.log("\nChallenge 2.0");
+
+//TODO: Enter your code here!
+
+
+/* Challenge 3 */
+console.log("\nChallenge 3.0");
+
+//TODO: Enter your code here!
+
+
+/* Challenge 4 */
+console.log("\nChallenge 4.0");
+
+//TODO: Enter your code here!
+
+
+/* ////////////////////////////////////////////////////////
+/* Challenge Set B
+*/ ////////////////////////////////////////////////////////
+console.log("\nChallenge Set B");
+
+console.log("\nChallenge 1a");
+
+/* Challenge 1a (forEach) */
+//TODO: Enter your code here!
+
+
+console.log("\nChallenge 1b");
+
+/* Challenge 1b (forEach) */
+//TODO: Enter your code here!
+
+
+
+
+/* Challenge 2
+
+Curry up now has a line wrapped around the truck!
+It takes about two minutes per delicious curry burrito.
+Output the customer's name & their expected wait time.
+*/
+
+console.log("\n\nChallenge 2.0");
+
+// forEach solution
+//TODO: Enter your code here!
+
+// for loop solution
+//TODO: Enter your code here!
